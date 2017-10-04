11f11eaf8de35ea355406ec21dcb2308eb56cf6e
Austin0077 <austinqweyu@gmail.com>
2017-10-04 13:14:43 +0300

    1.02 Release

---
+++ squrearry.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int aray[20];
+    int x,i;
+    for (x=1;x<=20;x++)
+    {
+        aray[i]=aray[i++];
+    }
+    for(i=0;i<20;i++)
+    {
+        cout<<i<<endl;
+    }
+}
