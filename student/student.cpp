fa89d020bdf8349f3406d0b6a8edb53a40a3fa00
Austin0077 <austinqweyu@gmail.com>
2017-10-17 23:06:24 +0300

    oopoop

---
+++ student/student.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+#include"student.h"
+
+from namespace std;
+
+student::display()
+{
+    cout<<"Name is: "<<name<<endl;
+    cout<<"Adm_No : "<<stdid<<endl;
+    cout<<"Grade  : "<<average<<endl;
+}
---
+++ student/student.h
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<string>
+
+from namespace std;
+class student
+{
+private:
+    int stdid;
+    string name;
+    double average;
+public:
+    void display();
+};
